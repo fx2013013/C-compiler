69c7dd6efabee01c6f91c4b3f27578b44e43a20a
fycy <1517286910@qq.com>
2022-01-06 14:35:51 +0800

    first commit

---
+++ test.c
@@ -0,0 +1,12 @@
+int main()
+{
+  int a;//变量a
+  int b;/*
+
+           定义变量*/#
+  a = 10;
+  b = a + 41;
+  if(a!=0)
+    print(b);
+  return 0;
+}
---
+++ uptoken.sh
@@ -0,0 +1,3 @@
+flex token.l
+gcc lex.yy.c -o token
+rm lex.yy.c
